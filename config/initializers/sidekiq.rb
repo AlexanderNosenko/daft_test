7278218cca3b71398f792679425a68727c500320
Alexander Nosenko <sania.ace@gmail.com>
2020-01-21 22:51:54 +0200

    Upgrade ModuleScaffold

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], namespace: "passionate_task_sidekiq_#{Rails.env}" }
+  config.redis = { url: ENV['REDIS_URL'], namespace: "daft_task_sidekiq_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], namespace: "passionate_task_sidekiq_#{Rails.env}" }
+  config.redis = { url: ENV['REDIS_URL'], namespace: "daft_task_sidekiq_#{Rails.env}" }
 end
